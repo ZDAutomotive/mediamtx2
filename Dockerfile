78854910f220c6e55942b380a30dce9edc8643fe
Yi <xyxy0202@qq.com>
2024-09-03 16:38:10 +0800

    support axis

--- Dockerfile
+++ Dockerfile
@@ -4,8 +4,8 @@ WORKDIR /build
 
 ENV GOPROXY=https://goproxy.cn,direct
 COPY . .
-# RUN go generate ./...
-RUN go build
+RUN go generate ./...
+RUN CGO_ENABLED=0 go build .
 
 
 FROM alpine
--- build.sh
+++ build.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-NAME=mediamtx:1.8.4
+NAME=mediamtx:1.8.4-axis
 REGISTRY=test.zddata.cloud:8443/cloud
 
-docker build -t $NAME .
+sudo docker build -t $NAME .
 # 登录
-docker login $REGISTRY
+sudo docker login $REGISTRY
 # 为本地镜像添加标签
-docker tag $NAME $REGISTRY/$NAME
+sudo docker tag $NAME $REGISTRY/$NAME
 # 推送
-docker push $REGISTRY/$NAME
+sudo docker push $REGISTRY/$NAME
